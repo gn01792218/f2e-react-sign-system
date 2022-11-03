6809448a2e65e1a6b4ca17c09159a49fc8eea981
jacky <jacky@metallurgietech.com>
2022-11-03 09:50:59 +0800

    添加滑鼠手寫板(功能未全，手寫位置偏移)

--- src/components/canvas/SignCanvas.tsx
+++ src/components/canvas/SignCanvas.tsx
@@ -1,20 +1,36 @@
-import { useRef, useState } from "react"
+import { MouseEventHandler, useRef, useState } from "react"
 import { BtnType } from '../../types/gloable'
 import Btn from '../btn/Btn'
 import useMouse from '../../hook/useMouse'
 function SignCanvas() {
-    const { getMousePos } = useMouse()
+    const { getCanvasMousePos } = useMouse()
     const signCanvas = useRef<HTMLCanvasElement>(null)
     const ctx = signCanvas.current?.getContext("2d")
 
     const [drawing,setDrawing] = useState(true)
 
     //methods
+   function handleMouseDown(event:MouseEvent) {
+        setDrawing(true)
+        let [x,y] = getCanvasMousePos(signCanvas.current!,event)
+        if(ctx) {
+            ctx.beginPath()
+            ctx.moveTo(x,y)
+            event.preventDefault()
+        }
+    }
     function handleMouseMove (event:MouseEvent) {
         if(!drawing) return
-        let [x,y] = getMousePos(event)
+        let [x,y] = getCanvasMousePos(signCanvas.current!,event)
         if(ctx){
             ctx.lineWidth = 2
+            ctx.lineCap = 'round'
+            ctx.lineJoin = 'round'
+            ctx.strokeStyle = 'yellow'
+            ctx.shadowBlur = 1
+            ctx.shadowColor = 'white'
+            ctx.lineTo(x,y)  //紀錄滑鼠移動到的x,y
+            ctx.stroke()  //將兩點連成線的方法
         }
     }
     function clearCanvas(){
@@ -25,6 +41,7 @@ function SignCanvas() {
             <canvas 
                 ref={signCanvas} 
                 className="signCanvas"
+                onMouseDown={handleMouseDown}
                 onMouseMove={handleMouseMove}
                 onTouchEnd={()=>setDrawing(false)} //手機觸碰離開時停止畫畫
                 onMouseUp={()=>setDrawing(false)}  //滑鼠鬆開時停止畫畫
--- src/hook/useMouse.ts
+++ src/hook/useMouse.ts
@@ -4,9 +4,16 @@ export default function useMouse(){
     function getMousePos(event:MouseEvent) {
         return [event.clientX,event.clientY]
     }
-
+    function getCanvasMousePos(canvas:HTMLCanvasElement,event:MouseEvent) {
+        let [x,y] = [event.clientX,event.clientY]
+        const rect = canvas.getBoundingClientRect()
+        x -= rect.left
+        y -= rect.top
+        return [x,y]
+    }
     return {
         //methods
-        getMousePos
+        getMousePos,
+        getCanvasMousePos,
     }
 }
