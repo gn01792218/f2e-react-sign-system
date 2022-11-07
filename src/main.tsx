8b252703e7e16bd5950c0eb5d28bc0b3de4658f0
MSI\gn01792218 <gn01792218@hotmail.com>
2022-11-07 22:12:00 +0800

    新增路由basename防止重刷404

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,8 @@ import App from './App'
 //react 18 用法
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 .render(
-    <BrowserRouter>
+    // 給路由+上basename(對應config檔案裡的base)，防止重刷404
+    <BrowserRouter basename='/f2e-react-sign-system/'>
         <Provider store={store}>
             <App />
         </Provider>
