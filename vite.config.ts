a20a5dea3b0493938496e914fbf747469f00b5b7
jacky <jacky@metallurgietech.com>
2022-11-07 08:56:07 +0800

    更換部署gh-page方式採用VITE的方式

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ const {resolve} = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/f2e-react-sign-system/',
   plugins: [react()],
   define:{
     'process.env':{}
