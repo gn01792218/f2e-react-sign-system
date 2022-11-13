dbccfb8ef7de86decb42f7052f6f71dc0cb9bafd
MSI\gn01792218 <gn01792218@hotmail.com>
2022-11-13 21:02:06 +0800

    暫時將子路由改成靜態引入

--- src/router/Routers.tsx
+++ src/router/Routers.tsx
@@ -1,13 +1,16 @@
 import { lazy, Suspense } from 'react'
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "../views/LandingPage"
+import SignPageStep1 from '../views/SignPageStep1';
+import SignPageStep2 from '../views/SignPageStep2';
+import SignPageStep3 from '../views/SignPageStep3';
 function Routers() {
     //路遊懶加載
     const CreateSignPage = lazy(()=>import('../views/CreateSignPage'))
     const SignPage = lazy(()=>import('../views/SignPage'))
-    const SignPageStep1 = lazy(()=>import(/* @vite-ignore */'../views/SignPageStep1'))
-    const SignPageStep2 = lazy(()=>import(/* @vite-ignore */'../views/SignPageStep2'))
-    const SignPageStep3 = lazy(()=>import(/* @vite-ignore */'../views/SignPageStep3'))
+    // const SignPageStep1 = lazy(()=>import(/* @vite-ignore */'../views/SignPageStep1'))
+    // const SignPageStep2 = lazy(()=>import(/* @vite-ignore */'../views/SignPageStep2'))
+    // const SignPageStep3 = lazy(()=>import(/* @vite-ignore */'../views/SignPageStep3'))
     return (
         <Suspense fallback={<div>Loading</div>}>
             <Routes>
