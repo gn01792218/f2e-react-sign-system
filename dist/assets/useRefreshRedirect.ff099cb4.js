76e1289e1d9c03773c5fed70ff231bf3eb61a04b
MSI\gn01792218 <gn01792218@hotmail.com>
2022-11-13 17:36:40 +0800

    部署

--- dist/assets/useRefreshRedirect.ff099cb4.js
+++
@@ -1,1 +0,0 @@
-import{d as r,f as a,r as s}from"./index.73aa8341.js";function c(t,o={}){const u=r(),e=a();return window.onbeforeunload=()=>(localStorage.setItem("routeKey",e.key),"\u8CC7\u6599\u5C07\u4E0D\u6703\u70BA\u60A8\u4FDD\u7559"),s.exports.useEffect(()=>{localStorage.getItem("routeKey")===e.key&&u(t,o)},[e]),{}}export{c as u};
--- src/views/SignPageStep3.tsx
+++ src/views/SignPageStep3.tsx
@@ -4,9 +4,15 @@ import MergeImageCanvas from '../components/canvas/MergeImageCanvas'
 import useSign from '../hook/useSign'
 import useRefreshRedirect from '../hook/useRefreshRedirect'
 import useImageMergeCanvas from '../hook/useImageMergeCanvas'
+import { useAppSelector } from '../store/hooks'
+
 function SignPageStep3() {
     const { backStep } = useSign()
     const { mergeImageCanvasRef, downloadMergeImage } = useImageMergeCanvas()
+
+    //Redux
+    const handSignImg = useAppSelector(state => state.createSign.handMadeSignImg)
+    const BgSrc = useAppSelector(state => state.createSign.BGImg)
     //重刷自動導向到Step1
     useRefreshRedirect('/SignPage/Step1')
     return (
@@ -16,7 +22,14 @@ function SignPageStep3() {
                 <Btn btnObj={{type:BtnType.PRIMARY,title:'上一步',clickHandler:()=>backStep('/SignPage/Step2')}}/>
             </div>
             <MergeImageCanvas mergeImageCanvasObj={{mergeImageCanvasRef}}/>
-            <Btn btnObj={{type:BtnType.PRIMARY,title:'下載簽署檔',clickHandler:downloadMergeImage}}/>
+            {
+                (handSignImg || BgSrc)? <Btn btnObj={{type:BtnType.PRIMARY,title:'下載文件',clickHandler:downloadMergeImage}}/> :
+                <div>
+                    <p>您尚未上傳文件及簽名檔</p>
+                </div>
+                
+            }
+            
         </section>
     )
 }
