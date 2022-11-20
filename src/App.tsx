2e25c99aa254a8bc0a539641f0f9e194d7f939b3
MSI\gn01792218 <gn01792218@hotmail.com>
2022-11-20 17:10:27 +0800

    建立HomeIcon元件

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import Routers from './router/Routers'
 import TheHeader from './components/TheHeader'
 import Prompt from './components/Prompt'
 import MsgBox from './components/MsgBox'
+import HomeIcon from './components/HomeIcon'
 import { Status } from './types/gloable'
 import { useAppSelector } from './store/hooks'
-import useImageUtil from "./hook/useImageUtil";
+
 function App() {
   window.onerror = function (msg,url,line,error) {
     console.log(msg,url,line,error)
   }
-  //hook
-  const { getAssetsFileURL } = useImageUtil()
+  
   //Redux
   const showMsgBox = useAppSelector( state => state.msgBox.msgBoxObj )
   const promptObj = useAppSelector( state => state.prompt.promptObj)
@@ -19,11 +19,8 @@ function App() {
       <Prompt promptObj={promptObj}/>
       <MsgBox msgBoxObj={{type:Status.SUCCESS,title:showMsgBox.title,message:showMsgBox.message,show:showMsgBox.show}}/>
       <div>
-        <div className="flex flex-col items-center justify-center">
-          <img src={getAssetsFileURL('images/icon.png')} width='150' height='150' alt="急急如律令，任意速速簽" />
-          <p className='mt-[-20px]'>急急如律令，任意速速簽</p>
-        </div>
-        <TheHeader/>
+        <HomeIcon />
+        <TheHeader />
       </div>
       <Routers />
     </div>
---
+++ src/components/HomeIcon.tsx
@@ -0,0 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import useImageUtil from "../hook/useImageUtil";
+
+function HomeIcon() {
+    //hook
+  const { getAssetsFileURL } = useImageUtil()
+  const navigate = useNavigate()
+    return (
+        <div className="flex flex-col items-center justify-center cursor-pointer" onClick={()=>navigate('/')}>
+          <img src={getAssetsFileURL('images/icon.png')} width='150' height='150' alt="急急如律令，任意速速簽" />
+          <p className='mt-[-20px]'>急急如律令，任意速速簽</p>
+        </div>
+    )
+}
+export default HomeIcon
