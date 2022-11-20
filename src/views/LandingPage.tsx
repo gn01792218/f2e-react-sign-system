0ae873190e2f42f95df0bece0f772434e993eb1d
MSI\gn01792218 <gn01792218@hotmail.com>
2022-11-20 17:04:36 +0800

    建立LandingPage

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -1,9 +1,20 @@
+import { useNavigate } from 'react-router-dom'
 import useImageUtil from '../hook/useImageUtil'
+import useSignStep from '../hook/useSignStep'
 function LandingPage() {
     const { getAssetsFileURL } = useImageUtil()
+    const { toStep } = useSignStep()
+    const navigate = useNavigate()
     return (
-        <main className='text-white'>
-            
+        <main className='text-white flex flex-col justify-center items-center lg:flex-row lg:h-[600px]'>
+            <section className='landingPage-card' onClick={()=>toStep('/SignPage/Step1',1)}>
+                <img src={getAssetsFileURL('images/sign.png')} alt="開始簽名" />
+                <h2>開始簽名</h2>
+            </section>
+            <section className='landingPage-card' onClick={()=>navigate('/CreateSign')}>
+                <img src={getAssetsFileURL('images/plusSign.png')} alt="建立簽名檔案" />
+                <h2>建立簽名檔案</h2>
+            </section>
         </main>
     )
 }
